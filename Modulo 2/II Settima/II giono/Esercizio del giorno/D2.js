4bab5a05d173559eb1163a3dcd2536a5cc4a9f51
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-04-30 14:35:24 +0200

    0

---
+++ Modulo 2/II Settima/II giono/Esercizio del giorno/D2.js
@@ -0,0 +1,252 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let num1=70;
+let num2=56;
+
+if (num1>num2){
+  console.log(" num1 è il numero più grande")
+}
+ else {
+  console.log("num2 è il numero più grande")
+ }
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 2
+  Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let numero1= 8;
+let numero5= 5;
+
+if (numero1=== numero5){
+  console.log ( "numero1 e numero2 sono uguali ")
+}
+
+else { console.log ( "not equal" )}
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 3
+  Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let number1= 100;
+
+if  (number1%5 == 0) {
+      console. log ("divisibile per 5");
+  }
+  else {
+     console.log ("non è divisibile per 5")}
+     
+
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 4
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numb1=8;
+let numb2= 5;
+
+if ((numb1==8) || (numb2==8) (numb1+numb2==8) || (numb1-numb2==8))
+{
+  console.log ("verificato");
+}
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 5
+  Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+  C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+  Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let totalShoppingCart= 45;
+
+if (totalShoppingCart>50){
+  console.log( "spedizione grtuita", totalShoppingCart)
+}
+
+else {
+  totalShoppingCart=totalShoppingCart+10;
+  console.log("paghi 10 €  in più ", totalShoppingCart)
+}
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 6
+  Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let prezzoScontato;
+if (totalShoppingCart>50){
+  prezzoScontato= totalShoppingCart-((totalShoppingCart *20)/100);
+  console.log( "spedizione grtuita il totale è ", prezzoScontato )
+}
+
+else {
+  prezzoScontato= ((totalShoppingCart /100)*20);
+  totalShoppingCart= totalShoppingCart-prezzoScontato;
+  totalShoppingCart= totalShoppingCart +10;
+  console.log(totalShoppingCart);
+}
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 7
+  Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
+  Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
+  Alla fine mostra il risultato in console.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let variabile1= 45;
+let variabile2 = 50;
+let  variabile3= 20;
+
+console.log( variabile1, variabile2, variabile3)
+
+if ((  variabile1 > variabile2) && (variabile1>variabile3))
+{
+      if (variabile2>variabile3)
+        console.log( variabile1, variabile2, variabile3)
+      else 
+      console.log (variabile1, variabile3, variabile2)
+      }
+
+else if ((  variabile2 > variabile1) && (variabile2>variabile3))
+{
+      if (variabile1>variabile3)
+      console.log( variabile2, variabile1, variabile3)
+       else 
+       console.log (variabile2, variabile3, variabile1)
+}
+
+else 
+{
+  if (variabile2>variabile1)
+  console.log( variabile3, variabile2, variabile1)
+   else 
+   console.log (variabile3, variabile1, variabile2)
+}
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 8
+  Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let valore= "pippo";
+
+if (typeof valore === "number")
+  console.log("Il valore è un numero.");
+
+  else {
+    console.log("Il valore non è un numero.");}
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 9
+  Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numero = 7;
+
+if (numero % 2 === 0) {
+    console.log(numero + " è un numero pari.");
+} else {
+    console.log(numero + " è un numero dispari.");
+}
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ ESERCIZIO 10
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
+  let val = 7
+  if (val < 10 ) {
+      console.log("Meno di 10");
+    } else if (val < 5) {
+      console.log("Meno di 5");
+    } else {
+      console.log("Uguale a 10 o maggiore");
+    }
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let val = 7
+  if  (val < 5) {
+      console.log("Meno di 5");
+    } 
+       
+    else if (val < 10 ) {
+      console.log("Meno di 10 ");
+    } 
+    else {
+      console.log("Uguale a 10 o maggiore");
+    }
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ESERCIZIO 11
+  Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
+*/
+
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+}
+
+me. city= "Toronto";
+console.log(me)
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 12
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
+  
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+delete me.lastName;
+
+
+    console.log(me) 
+
+/* ESERCIZIO 13
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+me.skills.pop();
+    console.log(me)
+
+/* ESERCIZIO 14
+  Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+var numeriArray= [ ];
+numeriArray.push(1);
+numeriArray.push(2)
+numeriArray.push(3)
+numeriArray.push(4)
+numeriArray.push(5)
+numeriArray.push(6)
+numeriArray.push(7)
+numeriArray.push(8)
+numeriArray.push(9)
+numeriArray.push(10)
+console.log( numeriArray);
+
+/* ESERCIZIO 15
+  Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
+*/
+
+
+/* SCRIVI QUI LA TUA RISPOSTA*/
+
+numeriArray.pop();  
+numeriArray.push("100")
+console.log (numeriArray)
---
+++ Modulo 2/II Settima/III giorno/Esercizio del giorno/U1-W2-D3 (1)/U1-W2-D3/D3-Solution/D3-Solution.js
@@ -0,0 +1,300 @@
+const starWarsCharacters = [
+  {
+    name: 'Luke Skywalker',
+    height: '172',
+    mass: '277',
+    hair_color: 'blond',
+    skin_color: 'fair',
+    eye_color: 'blue',
+    birth_year: '19BBY',
+    gender: 'male',
+  },
+  {
+    name: 'C-3PO',
+    height: '167',
+    mass: '75',
+    hair_color: 'n/a',
+    skin_color: 'gold',
+    eye_color: 'yellow',
+    birth_year: '112BBY',
+    gender: 'n/a',
+  },
+  {
+    name: 'R2-D2',
+    height: '96',
+    mass: '32',
+    hair_color: 'n/a',
+    skin_color: 'white, blue',
+    eye_color: 'red',
+    birth_year: '33BBY',
+    gender: 'n/a',
+  },
+  {
+    name: 'Darth Vader',
+    height: '202',
+    mass: '136',
+    hair_color: 'none',
+    skin_color: 'white',
+    eye_color: 'yellow',
+    birth_year: '41.9BBY',
+    gender: 'male',
+  },
+  {
+    name: 'Leia Organa',
+    height: '150',
+    mass: '49',
+    hair_color: 'brown',
+    skin_color: 'light',
+    eye_color: 'brown',
+    birth_year: '19BBY',
+    gender: 'female',
+  },
+  {
+    name: 'Owen Lars',
+    height: '178',
+    mass: '120',
+    hair_color: 'brown, grey',
+    skin_color: 'light',
+    eye_color: 'blue',
+    birth_year: '52BBY',
+    gender: 'male',
+  },
+  {
+    name: 'Beru Whitesun lars',
+    height: '165',
+    mass: '75',
+    hair_color: 'brown',
+    skin_color: 'light',
+    eye_color: 'blue',
+    birth_year: '47BBY',
+    gender: 'female',
+  },
+  {
+    name: 'R5-D4',
+    height: '97',
+    mass: '32',
+    hair_color: 'n/a',
+    skin_color: 'white, red',
+    eye_color: 'red',
+    birth_year: 'unknown',
+    gender: 'n/a',
+  },
+  {
+    name: 'Biggs Darklighter',
+    height: '183',
+    mass: '84',
+    hair_color: 'black',
+    skin_color: 'light',
+    eye_color: 'brown',
+    birth_year: '24BBY',
+    gender: 'male',
+  },
+  {
+    name: 'Obi-Wan Kenobi',
+    height: '182',
+    mass: '77',
+    hair_color: 'auburn, white',
+    skin_color: 'fair',
+    eye_color: 'blue-gray',
+    birth_year: '57BBY',
+    gender: 'male',
+  },
+]
+
+/* ESERCIZIO 1
+  Crea una variabile chiamata "charactersNames" e assegnale un array vuoto
+*/
+
+const characters = []
+
+/* ESERCIZIO 2
+  Utilizzando un ciclo for, cicla l'array "starWarsCharacters". 
+  Dovrai accedere alla proprietà "name" di ogni oggetto in esso contenuto, e inserirla nell'array "starWarsCharacters" creato precedentemente.
+  Come risultato dovresti ottenere qualcosa di simile: ["Luke Skywalker", "C-3PO", "R2-D2", etc..]
+*/
+
+for (let i = 0; i < starWarsCharacters.length; i++) {
+  const character = starWarsCharacters[i]
+
+  characters.push(character.name)
+}
+
+console.log(characters)
+
+/* ESERCIZIO 3
+  Seguendo i passaggi precedenti crea un nuovo array chiamato "femaleCharacters" e inserisci al suo interno
+  tutti gli oggetto femminili, CON QUESTA STRUTTURA: 
+  {name: Leia Organa, hair_color: "brown", eye_color: "brown"}
+*/
+
+const femaleCharacters = []
+
+for (let i = 0; i < starWarsCharacters.length; i++) {
+  const character = starWarsCharacters[i]
+
+  if (character.gender === 'female') {
+    const characterObj = {
+      name: character.name,
+      hair_color: character.hair_color,
+      eye_color: character.eye_color,
+    }
+    femaleCharacters.push(characterObj)
+  }
+}
+
+console.log(femaleCharacters)
+
+/* ESERCIZIO 4
+  Crea un oggetto "eyeColor" che abbia le seguenti proprietà: blue, yellow, brown, red, blue-gray.
+  Ad ognuna di queste proprietà assegna come valore un array vuoto.
+*/
+const eyeColor = {
+  blue: [],
+  yellow: [],
+  brown: [],
+  red: [],
+  'blue-gray': [],
+}
+
+/* ESERCIZIO 5
+  Utilizza uno switch statement per inserire uno ad uno gli oggetti dei personaggi di "starWarsCharacters" negli array relativi al colore degli occhi precedentemente creati.
+  Ogni personaggio dovrà finire nell'array corrispondente al suo colore degli occhi (al valore della sua proprietà "eye_color").
+*/
+
+for (let i = 0; i < starWarsCharacters.length; i++) {
+  const character = starWarsCharacters[i]
+
+  switch (character.eye_color) {
+    case 'blue':
+      eyeColor.blue.push(character)
+      break
+    case 'yellow':
+      eyeColor.yellow.push(character)
+      break
+    case 'brown':
+      eyeColor.brown.push(character)
+      break
+    case 'red':
+      eyeColor.red.push(character)
+      break
+    case 'blue-gray':
+      eyeColor['blue-gray'].push(character)
+      break
+  }
+}
+console.log(eyeColor)
+
+/* ESERCIZIO 6
+  Usa un while loop per calcolare la massa totale dell'equipaggio. Salvala in una variabile chiamata "crewMass".
+*/
+
+let index = 0
+let crewMass = 0
+
+while (index < starWarsCharacters.length) {
+  const character = starWarsCharacters[index]
+  crewMass = crewMass + parseInt(character.mass)
+  index++
+}
+
+console.log(crewMass)
+
+/* ESERCIZIO 7
+
+/* ESERCIZIO 7
+  Crea uno switch statement per rivelare la tipologia di carico, utilizzando la massa totale, di un'impotetica astronave contenente i personaggi dell'array "starWarsCharacters"
+  (sei in difficoltà? cerca su un motore di ricerca switch case e conditionals)
+
+  Se la massa è inferiore a 500 stampa in console: "Ship is under loaded"
+  Se la massa è superiore a 500 e inferiore a 700 stampa in console: "Ship is half loaded"
+  Se la massa è superiore a 700 e inferiore a 900 stampa in console: "Warning: Load is over 700"
+  Se la massa è superiore a 900 e inferiore a 1000 stampa in console: "Critical Load: Over 900"
+  Se la massa è superiore a 1000 stampa in console: "DANGER! OVERLOAD ALERT: escape from ship now!"
+
+  Una volta fatto, modifica la massa di qualche elemento dell'equipaggio e vedi se riesci ad ottenere un messaggio diverso.
+*/
+
+switch (true) {
+  case crewMass < 500:
+    console.log('Ship is under loaded')
+    break
+  case crewMass > 500 && crewMass < 700:
+    console.log('Ship is half loaded')
+    break
+  case crewMass > 700 && crewMass < 900:
+    console.log('Warning: Load is over 700')
+    break
+  case crewMass > 900 && crewMass < 1000:
+    console.log('Critical Load: Over 900')
+    break
+  case crewMass > 1000:
+    console.log('DANGER! OVERLOAD ALERT: escape from ship now!')
+    break
+  default:
+    console.log('No data provided')
+}
+
+/* ESERCIZIO 8
+  Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
+*/
+
+for (let i = 0; i < starWarsCharacters.length; i++) {
+  const character = starWarsCharacters[i]
+
+  if (character.gender === 'n/a') {
+    character.gender = 'robot'
+  }
+}
+
+console.log(starWarsCharacters)
+
+/* --EXTRA-- ESERCIZIO 9
+  Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "charactersNames" le stringhe corrispondenti a personaggi con lo stesso nome.
+  Usa uno più for loop per raggiungere il risultato
+
+  (tip: cerca un metodo degli array per rimuovere un elemento)
+
+  Una volta fatto crea un console.log per controllare la proprietà length di "charactersNames" prima e dopo l'operazione.
+*/
+console.log('CHARACTERS BEFORE', characters.length)
+
+for (let i = 0; i < characters.length; i++) {
+  const character = characters[i]
+
+  for (let j = 0; j < femaleCharacters.length; j++) {
+    const femCharacter = femaleCharacters[j]
+
+    if (femCharacter.name === character) {
+      console.log('FEMALE', character)
+      characters.splice(i, 1)
+    }
+  }
+}
+
+console.log('CHARACTERS AFTER', characters.length)
+
+/* --EXTRA-- ESERCIZIO 10
+  Crea una funzionalità che prenda un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo (a tuo piacimento).
+*/
+
+const randomIndex = Math.floor(Math.random() * starWarsCharacters.length)
+const selectedCharacter = starWarsCharacters[randomIndex]
+
+console.log('The found character name is:', selectedCharacter.name)
+
+if (selectedCharacter.gender === 'female') {
+  console.log('She is', selectedCharacter.height, 'cm tall')
+} else {
+  console.log('He is', selectedCharacter.height, 'cm tall')
+}
+
+if (
+  selectedCharacter.hair_color !== 'n/a' &&
+  selectedCharacter.hair_color !== 'none'
+) {
+  console.log('and has', selectedCharacter.hair_color, 'hair,')
+} else {
+  console.log('and bald,')
+}
+
+console.log('with', selectedCharacter.skin_color, 'skin.')
