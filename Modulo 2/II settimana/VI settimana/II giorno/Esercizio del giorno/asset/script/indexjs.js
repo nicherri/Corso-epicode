8de5eb2dc62ce5e4516c4d9471f142174033cb03
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-05-02 11:01:29 +0200

    .

--- Modulo 2/II settimana/VI settimana/II giorno/Esercizio del giorno/asset/script/indexjs.js
+++ Modulo 2/II settimana/VI settimana/II giorno/Esercizio del giorno/asset/script/indexjs.js
@@ -1,10 +1,16 @@
-const ottineiAnno =  function ottieniAnno() {
+function ottieniAnno() {
     var dataCorrente = new Date();
     var anno = dataCorrente.getFullYear();
     return anno.toString();
 }
 
+// Chiamata alla funzione per ottenere l'anno corrente
+const annoCorrente = ottieniAnno();
+console.log("Anno corrente:", annoCorrente); // Stampa l'anno corrente nella console
+
 // Ottieni il riferimento allo span
 var spanAnno = document.getElementById("anno");
+console.log("Span:", spanAnno); // Stampa il riferimento allo span nella console
+
 // Aggiorna il contenuto dello span con l'anno corrente
-spanAnno.textContent = ottieniAnno();
+spanAnno.textContent = annoCorrente;
