b78bc952c62adfa95096c5318bc9d553953cd9ce
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-06-17 14:07:21 +0200

    rifaccio la cartella
    mi creo il primo modulo backend

---
+++ Frontend/Modulo 2/II settimana/II giorno/Esercizio del giorno/asset/script/indexjs.js
@@ -0,0 +1,3 @@
+const spanToAddYearInto = document.getElementById('currentYear')
+const currentYear = new Date().getFullYear() // 2024
+spanToAddYearInto.innerText = currentYear
--- Modulo 2/II settimana/II giorno/Esercizio del giorno/asset/script/indexjs.js
+++
@@ -1,3 +0,0 @@
-const spanToAddYearInto = document.getElementById('currentYear')
-const currentYear = new Date().getFullYear() // 2024
-spanToAddYearInto.innerText = currentYear
