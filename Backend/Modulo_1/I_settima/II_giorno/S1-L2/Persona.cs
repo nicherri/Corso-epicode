e07e4fc0aa55ead449b64338754912a03f689d28
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-06-18 14:42:00 +0200

    esercixio giorno 2

---
+++ Backend/Modulo_1/I_settima/II_giorno/S1-L2/Persona.cs
@@ -0,0 +1,36 @@
+﻿namespace S1_L2
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+        public int Eta { get; set; }
+        public Persona(string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        public int GetEta()
+        {
+            return Eta;
+        }
+
+        public string GetDettagli()
+        {
+            return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Eta}";
+        }
+    }
+}
---
+++ Backend/Modulo_1/I_settima/II_giorno/S1-L2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace S1_L2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Persona persona = new Persona("Mario", "Rossi", 30);
+
+             
+                Console.WriteLine("Nome: " + persona.GetNome());
+                Console.WriteLine("Cognome: " + persona.GetCognome());
+                Console.WriteLine("Età: " + persona.GetEta());
+                Console.WriteLine("Dettagli: " + persona.GetDettagli());
+
+               
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
